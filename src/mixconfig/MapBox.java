65231032f03efe6e7b557c86a21ce3ccaac43073
am20 <am20@8653a64e-80b3-454b-abe1-51d189067293>
2003-02-25 02:46:02 +0000

    Frame statt JFrame verlangen.

--- src/mixconfig/MapBox.java
+++ src/mixconfig/MapBox.java
@@ -33,7 +33,7 @@ class MapBox extends JDialog implements ActionListener,ChangeListener
     private JSlider s;
     private JPanel p;
 
-    public MapBox(JFrame parent,String lat,String lon,int level) throws Exception
+    public MapBox(Frame parent,String lat,String lon,int level) throws Exception
     {
       super(parent,"",false);
       setBackground(Color.white);
