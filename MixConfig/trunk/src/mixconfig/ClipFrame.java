77d2efebe144406efa3aeab227b917fb69e2ebdb
am20 <am20@8653a64e-80b3-454b-abe1-51d189067293>
2003-05-07 04:30:45 +0000

    Feature eingebaut, womit man mehrere Daten auf einmal anbieten kann.

--- MixConfig/trunk/src/mixconfig/ClipFrame.java
+++ MixConfig/trunk/src/mixconfig/ClipFrame.java
@@ -1,19 +1,46 @@
 package mixconfig;
 
+import java.awt.Choice;
 import java.awt.Dialog;
 import java.awt.TextArea;
 import java.awt.Button;
 import java.awt.BorderLayout;
 import java.awt.event.*;
+
 import javax.swing.JOptionPane;
 
-class ClipFrame extends Dialog implements ActionListener
+class ClipFrame extends Dialog implements ActionListener, ItemListener
 {
   private TextArea m_TextArea;
+  private Choice chooser;
+  private ClipChoice[] choices;
 
-  public ClipFrame(String title,boolean open)
+  static public class ClipChoice
   {
-    super(TheApplet.getMainWindow(),title,true);
+      public String name;
+      public String text;
+      
+      public ClipChoice(String n, String t)
+      {
+          name = n;
+          text = t;
+      }
+  }
+  
+  private void init(boolean open, ClipChoice[] c)
+  {
+    choices = c;
+    if(choices==null)
+        chooser = null;
+    else
+    {
+       chooser = new Choice();
+       for(int i=0;i<choices.length;i++)
+           chooser.add(choices[i].name);
+       add(chooser, BorderLayout.NORTH);
+       chooser.addItemListener(this);
+    }
+   
     m_TextArea = new TextArea(30,80);
     m_TextArea.setText("");
     add(m_TextArea,BorderLayout.CENTER);
@@ -35,6 +62,18 @@ class ClipFrame extends Dialog implements ActionListener
     });
 
     pack();
+  }
+
+  public ClipFrame(String title,boolean open, ClipChoice[] choices)
+  {
+    super(TheApplet.getMainWindow(),title,true);
+    init(open, choices);
+  }
+
+  public ClipFrame(String title,boolean open)
+  {
+      super(TheApplet.getMainWindow(),title,true);
+      init(open, null);
   }
 
   public void setText(String data)
@@ -64,5 +103,9 @@ class ClipFrame extends Dialog implements ActionListener
       }
     }
 
+    public void itemStateChanged(ItemEvent e)
+    {
+        setText(choices[chooser.getSelectedIndex()].text);
+    }
 
 }
