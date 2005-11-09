5e25cb92ad6b236ea74469f762a847ff14c353de
rolf <rolf@8653a64e-80b3-454b-abe1-51d189067293>
2005-11-09 14:57:00 +0000

    <Kein Kommentar eingegeben>

--- MixConfig/trunk/src/mixconfig/CertDetailsDialog.java
+++ MixConfig/trunk/src/mixconfig/CertDetailsDialog.java
@@ -30,19 +30,25 @@ package mixconfig;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import anon.crypto.IMyPublicKey;
 import anon.crypto.JAPCertificate;
 import anon.crypto.X509DistinguishedName;
 import anon.crypto.X509Extensions;
-import gui.JAPDialog;
+import gui.GUIUtils;
 import anon.crypto.X509UnknownExtension;
 import anon.crypto.AbstractX509Extension;
+import org.bouncycastle.asn1.DERString;
 import java.util.Vector;
-import java.awt.Component;
+import gui.JAPDialog;
 
 /** This dialog shows the details of a certificate from the CertPanel.
  * @author Tobias Bayer
@@ -52,13 +58,12 @@ public class CertDetailsDialog extends JAPDialog
 	public CertDetailsDialog(Component a_parent, JAPCertificate a_cert)
 	{
 		super(a_parent, "Certificate Details");
-
-		/*
+		this.setSize(500, 400);
+		JPanel root = new JPanel();
 		JScrollPane scrollPane = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-							JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.setViewportView(getRootPanel());*/
-
-		getRootPanel().setLayout(new GridBagLayout());
+												 JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setViewportView(root);
+		this.getContentPane().setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.weighty = 1;
 		gbc.weightx = 1;
@@ -66,8 +71,8 @@ public class CertDetailsDialog extends JAPDialog
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		gbc.fill = gbc.BOTH;
-		//getRootPanel().add(scrollPane, gbc);
-		getRootPanel().setLayout(new GridBagLayout());
+		this.getContentPane().add(scrollPane, gbc);
+		root.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = c.NONE;
 		c.anchor = c.NORTHWEST;
@@ -76,36 +81,36 @@ public class CertDetailsDialog extends JAPDialog
 		c.gridy = 0;
 
 		//Subject
-		getRootPanel().add(new JLabel("Subject"), c);
+		root.add(new JLabel("Subject"), c);
 
 		c.insets = new Insets(5, 15, 5, 5);
 
 		X509DistinguishedName dname = new X509DistinguishedName(a_cert.getSubject().toString());
 
 		c.gridy++;
-		getRootPanel().add(new JLabel("Country:"), c);
+		root.add(new JLabel("Country:"), c);
 		c.gridx++;
-		getRootPanel().add(new JLabel(dname.getCountryCode()), c);
+		root.add(new JLabel(dname.getCountryCode()), c);
 		c.gridx = 0;
 		c.gridy++;
-		getRootPanel().add(new JLabel("City:"), c);
+		root.add(new JLabel("City:"), c);
 		c.gridx++;
-		getRootPanel().add(new JLabel(dname.getLocalityName()), c);
+		root.add(new JLabel(dname.getLocalityName()), c);
 		c.gridx = 0;
 		c.gridy++;
-		getRootPanel().add(new JLabel("Organisation:"), c);
+		root.add(new JLabel("Organisation:"), c);
 		c.gridx++;
-		getRootPanel().add(new JLabel(dname.getOrganisation()), c);
+		root.add(new JLabel(dname.getOrganisation()), c);
 		c.gridx = 0;
 		c.gridy++;
-		getRootPanel().add(new JLabel("Organisational Unit:"), c);
+		root.add(new JLabel("Organisational Unit:"), c);
 		c.gridx++;
-		getRootPanel().add(new JLabel(dname.getOrganisationalUnit()), c);
+		root.add(new JLabel(dname.getOrganisationalUnit()), c);
 		c.gridx = 0;
 		c.gridy++;
-		getRootPanel().add(new JLabel("Common Name:"), c);
+		root.add(new JLabel("Common Name:"), c);
 		c.gridx++;
-		getRootPanel().add(new JLabel(dname.getCommonName()), c);
+		root.add(new JLabel(dname.getCommonName()), c);
 		c.gridx = 0;
 
 		//Issuer
@@ -113,38 +118,38 @@ public class CertDetailsDialog extends JAPDialog
 
 		c.gridy++;
 		c.insets = new Insets(5, 5, 5, 5);
-		getRootPanel().add(new JLabel("Issuer"), c);
+		root.add(new JLabel("Issuer"), c);
 
 		c.insets = new Insets(5, 15, 5, 5);
 
 		c.gridy++;
-		getRootPanel().add(new JLabel("Organisational Unit:"), c);
+		root.add(new JLabel("Organisational Unit:"), c);
 		c.gridx++;
-		getRootPanel().add(new JLabel(dname.getOrganisationalUnit()), c);
+		root.add(new JLabel(dname.getOrganisationalUnit()), c);
 		c.gridx = 0;
 		c.gridy++;
-		getRootPanel().add(new JLabel("Organisation:"), c);
+		root.add(new JLabel("Organisation:"), c);
 		c.gridx++;
-		getRootPanel().add(new JLabel(dname.getOrganisation()), c);
+		root.add(new JLabel(dname.getOrganisation()), c);
 
 		//Public  Key
 		IMyPublicKey pkey = a_cert.getPublicKey();
 		c.gridx = 0;
 		c.gridy++;
 		c.insets = new Insets(5, 5, 5, 5);
-		getRootPanel().add(new JLabel("Public Key"), c);
+		root.add(new JLabel("Public Key"), c);
 		c.insets = new Insets(5, 15, 5, 5);
 
 		c.gridy++;
-		getRootPanel().add(new JLabel("Algorithm:"), c);
+		root.add(new JLabel("Algorithm:"), c);
 		c.gridx++;
-		getRootPanel().add(new JLabel(pkey.getAlgorithm()), c);
+		root.add(new JLabel(pkey.getAlgorithm()), c);
 
 		//Extensions
 		c.gridx = 0;
 		c.gridy++;
 		c.insets = new Insets(5, 5, 5, 5);
-		getRootPanel().add(new JLabel("Extensions"), c);
+		root.add(new JLabel("Extensions"), c);
 		c.insets = new Insets(5, 15, 5, 5);
 
 		X509Extensions extensions = a_cert.getExtensions();
@@ -154,7 +159,7 @@ public class CertDetailsDialog extends JAPDialog
 			c.gridx = 0;
 			c.gridy++;
 			AbstractX509Extension extension = extensions.getExtension(i);
-			getRootPanel().add(new JLabel(extension.getName()), c);
+			root.add(new JLabel(extension.getName()), c);
 			Vector v = extension.getValues();
 			c.gridx++;
 			for (int j = 0; j < v.size(); j++)
@@ -162,7 +167,7 @@ public class CertDetailsDialog extends JAPDialog
 				if (v.elementAt(j) instanceof String)
 				{
 					String extValue = (String) v.elementAt(j);
-					getRootPanel().add(new JLabel(extValue), c);
+					root.add(new JLabel(extValue), c);
 					c.gridy++;
 				}
 			}
@@ -170,11 +175,11 @@ public class CertDetailsDialog extends JAPDialog
 
 		c.gridx = 0;
 		c.gridy++;
-		getRootPanel().add(new JLabel("..."), c);
+		root.add(new JLabel("..."), c);
 		c.gridx++;
 		c.weightx = 1;
 		c.weighty = 1;
-		getRootPanel().add(new JLabel("..."), c);
+		root.add(new JLabel("..."), c);
 		c.gridx = 0;
 
 		//Ok Button
@@ -187,14 +192,10 @@ public class CertDetailsDialog extends JAPDialog
 			}
 		});
 		gbc.gridy++;
-		gbc.gridx = gbc.CENTER;
-		gbc.anchor = gbc.SOUTH;
-		gbc.weighty = 10;
-		gbc.weightx = 10;
+		gbc.anchor = gbc.NORTHEAST;
+		gbc.weighty = 0;
+		gbc.weightx = 0;
 		gbc.fill = gbc.NONE;
-		c.insets = new Insets (10, 10, 10, 10);
-		getRootPanel().add(okButton, gbc);
-
-		getInternalDialog().pack();
+		this.getContentPane().add(okButton, gbc);
 	}
 }
