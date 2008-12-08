59e9b30a66611464625aaf27bfd8a2aa77f904f6
rolf <rolf@8653a64e-80b3-454b-abe1-51d189067293>
2008-12-08 13:55:37 +0000

    added missing files

---
+++ src/gui/MixConfigTextField.java
@@ -0,0 +1,73 @@
+package gui;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.JTextField;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
+
+import logging.LogHolder;
+import logging.LogLevel;
+import logging.LogType;
+
+public class MixConfigTextField extends JTextField {
+
+	public MixConfigTextField()
+	{
+		super();
+	}
+	
+	public MixConfigTextField(int columns)
+	{
+		super(columns);		
+	}
+	
+	public MixConfigTextField(String text,int columns)
+	{
+		super(text, columns);
+	}
+	
+	protected final Document createDefaultModel()
+	{
+		return new MixConfigDocument();
+	}
+	
+	private final class MixConfigDocument extends PlainDocument
+	{
+		// Blacklist of characters that may not be entered
+		String sBlacklistedChars = "äöüÄÖÜ<>&";
+        String sRegexAllowedChar = "[^"+sBlacklistedChars+"]";
+        
+        // Compile blacklisted chars for replacing them in strings with length > 1
+		Pattern p = Pattern.compile("["+sBlacklistedChars+"]");
+        
+		public void insertString(int offSet, String string, AttributeSet attributeSet) 
+			throws BadLocationException
+		{
+			//LogHolder.log(LogLevel.DEBUG, LogType.GUI, "Current string: " + string);
+			
+			// Insert single characters if allowed
+			if (string.length() == 1 && string.matches(sRegexAllowedChar))
+			{
+				//LogHolder.log(LogLevel.DEBUG, LogType.GUI, "Inserting " + string);
+				super.insertString(offSet, string, attributeSet);				
+			} 
+			else if (string.length() > 1) 
+			{
+				// Replace blacklisted characters by whitespaces
+				Matcher m = p.matcher(string);
+				String newString = m.replaceAll("");
+				//LogHolder.log(LogLevel.DEBUG, LogType.GUI, "Inserting " + string);
+				super.insertString(offSet, newString, attributeSet);
+			}
+			else 
+			{
+				LogHolder.log(LogLevel.DEBUG,LogType.GUI, "Rejecting " + string);
+				return;
+			}
+		}
+	}
+}
---
+++ src/gui/dialog/LinkedEmailComposer.java
@@ -0,0 +1,58 @@
+package gui.dialog;
+
+import gui.dialog.JAPDialog.ILinkedInformation;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import mixconfig.panels.OwnCertificatesPanel;
+import mixconfig.tools.EmailComposer;
+
+public class LinkedEmailComposer implements ILinkedInformation {
+
+	String m_message = null;
+	OwnCertificatesPanel m_panel = null;
+	
+	public LinkedEmailComposer(String message, OwnCertificatesPanel panel) 
+	{
+		m_message = message;
+		m_panel = panel;
+	}
+	
+	public void clicked(boolean state) {
+		try {
+			EmailComposer.composeEmail();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String getMessage() 
+	{
+		return m_message;
+	}
+
+	public int getType() 
+	{
+		return 0;
+	}
+
+	public boolean isApplicationModalityForced() 
+	{
+		return false;
+	}
+
+	public boolean isCloseWindowActive() 
+	{
+		return false;
+	}
+
+	public boolean isOnTop() 
+	{
+		return false;
+	}
+}
