1b036d5eaaa54e1e9b26991fe87c7464cfce6ac7
rolf <rolf@8653a64e-80b3-454b-abe1-51d189067293>
2009-04-28 15:21:43 +0000

    small manual update

--- MixConfig/trunk/src/mixconfig/MixConfig.java
+++ MixConfig/trunk/src/mixconfig/MixConfig.java
@@ -84,7 +84,7 @@ public class MixConfig extends JApplet
 	public final static int FILTER_P10 = 32;
 	public final static int FILTER_B64_P10 = 64;
 
-	public final static String VERSION = "00.05.008";  //NEVER change the layout of this line!!
+	public final static String VERSION = "00.05.009";  //NEVER change the layout of this line!!
 	
 	private static final String IMG_MAIN = MixConfig.class.getName() + "_icon.gif";
 
